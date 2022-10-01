1cab14a3a07868eeea6fac5fc59bc8f473dc09ab
13prashant <pacific1392@gmail.com>
2022-10-01 16:28:39 +0530

    added gradient background to loginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -9,51 +9,53 @@ function LoginForm({ Login, error }) {
     Login(details);
   };
   return (
-    <form onSubmit={submitHandler}>
-      <div className="box-form">
-        <div className="left">
-          <div className="overlay">
-            <h1>WareHouse Royale</h1>
-            <span>
-              <p>Let's Trade!</p>
-            </span>
+    <div className="login-form">
+      <form onSubmit={submitHandler}>
+        <div className="box-form">
+          <div className="left">
+            <div className="overlay">
+              <h1>WareHouse Royale</h1>
+              <span>
+                <p>Let's Trade!</p>
+              </span>
+            </div>
+          </div>
+
+          <div className="right">
+            <div className="content">Login</div>
+
+            <div className="inputs">
+              <input
+                placeholder="username"
+                className="user_name"
+                type="email"
+                name="email"
+                id="email"
+                onChange={(e) =>
+                  setDetails({ ...details, email: e.target.value })
+                }
+                value={details.email}
+              />
+              <input
+                placeholder="password"
+                className="password"
+                type="password"
+                name="password"
+                id="password"
+                onChange={(e) =>
+                  setDetails({ ...details, password: e.target.value })
+                }
+                value={details.password}
+              />
+            </div>
+            {error != null ? <div className="error">{error}</div> : ''}
+            <div className="button_div">
+              <input className="ibutton" type="submit" value="LOGIN" />
+            </div>
           </div>
         </div>
-
-        <div className="right">
-          <div className="content">Login</div>
-
-          <div className="inputs">
-            <input
-              placeholder="username"
-              className="user_name"
-              type="email"
-              name="email"
-              id="email"
-              onChange={(e) =>
-                setDetails({ ...details, email: e.target.value })
-              }
-              value={details.email}
-            />
-            <input
-              placeholder="password"
-              className="password"
-              type="password"
-              name="password"
-              id="password"
-              onChange={(e) =>
-                setDetails({ ...details, password: e.target.value })
-              }
-              value={details.password}
-            />
-          </div>
-          {error != null ? <div className="error">{error}</div> : ''}
-          <div className="button_div">
-            <input className="ibutton" type="submit" value="LOGIN" />
-          </div>
-        </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 }
 
