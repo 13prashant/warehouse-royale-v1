97cebe256205feae7bbfc2800fa1d7b0c1951c7e
13prashant <pacific1392@gmail.com>
2022-10-10 21:55:07 +0530

    created recruitment-room page

--- src/components/ui/WarehouseButton.js
+++ src/components/ui/WarehouseButton.js
@@ -1,11 +1,17 @@
 import PropTypes from 'prop-types';
 
-export default function WarehouseButton({ text, sm, success, ...rest }) {
+export default function WarehouseButton({
+  text,
+  sm,
+  success,
+  warning,
+  ...rest
+}) {
   const warehouseButtonStyle = {
     border: 'none',
     borderRadius: '0.3rem',
     padding: `${sm ? '0.3125rem 1rem' : '0.625rem 1rem'}`,
-    backgroundColor: `${success ? '#3bb077' : '#3f51b5'}`,
+    backgroundColor: `${success ? '#3bb077' : warning ? '#ea1010' : '#3f51b5'}`,
     color: '#ffffff',
     cursor: 'pointer',
   };
--- src/components/ui/WarehouseCard.js
+++ src/components/ui/WarehouseCard.js
@@ -1,4 +1,4 @@
-export default function WarehouseCard({ children }) {
+export default function WarehouseCard({ children, ...rest }) {
   const warehouseCardStyle = {
     boxShadow:
       'rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.1) 0px 8px 10px -6px',
@@ -6,5 +6,9 @@ export default function WarehouseCard({ children }) {
     borderRadius: '1rem',
   };
 
-  return <div style={warehouseCardStyle}>{children}</div>;
+  return (
+    <div {...rest} style={warehouseCardStyle}>
+      {children}
+    </div>
+  );
 }
