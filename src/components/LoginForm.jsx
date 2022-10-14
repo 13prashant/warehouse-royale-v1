39e092b9e5ce3c28e9f8675cd2a00c97645e8a99
13prashant <pacific1392@gmail.com>
2022-10-14 07:38:28 +0530

    updated LoginForm

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Container from '@mui/system/Container';
 // Css
 import './../style/LoginForm.css';
 
@@ -9,53 +10,60 @@ function LoginForm({ Login, error }) {
     e.preventDefault();
     Login(details);
   };
+
   return (
-    <div className="login-form">
-      <form onSubmit={submitHandler}>
-        <div className="box-form">
-          <div className="left">
-            <div className="overlay">
-              <h1>WareHouse Royale</h1>
-              <span>
-                <p>Let's Trade!</p>
-              </span>
+    <div className="loginForm">
+      <Container>
+        <form onSubmit={submitHandler}>
+          <div className="loginForm__box">
+            <div className="loginForm__left">
+              <div className="loginForm__overlay"></div>
+              <div className="loginForm__title">
+                <h1>WareHouse Royale</h1>
+                <h4>Let's Trade!</h4>
+              </div>
+            </div>
+
+            <div className="loginForm__right">
+              <h1>Login</h1>
+              <div className="loginForm__inputs">
+                <input
+                  placeholder="username"
+                  className="user_name"
+                  type="email"
+                  name="email"
+                  id="email"
+                  onChange={(e) =>
+                    setDetails({ ...details, email: e.target.value })
+                  }
+                  value={details.email}
+                />
+                <input
+                  placeholder="password"
+                  className="password"
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={(e) =>
+                    setDetails({ ...details, password: e.target.value })
+                  }
+                  value={details.password}
+                />
+              </div>
+              {error != null ? (
+                <div className="loginForm__error">{error}</div>
+              ) : (
+                ''
+              )}
+              <input
+                className="loginForm__button"
+                type="submit"
+                value="LOGIN"
+              />
             </div>
           </div>
-
-          <div className="right">
-            <div className="content">Login</div>
-
-            <div className="inputs">
-              <input
-                placeholder="username"
-                className="user_name"
-                type="email"
-                name="email"
-                id="email"
-                onChange={(e) =>
-                  setDetails({ ...details, email: e.target.value })
-                }
-                value={details.email}
-              />
-              <input
-                placeholder="password"
-                className="password"
-                type="password"
-                name="password"
-                id="password"
-                onChange={(e) =>
-                  setDetails({ ...details, password: e.target.value })
-                }
-                value={details.password}
-              />
-            </div>
-            {error != null ? <div className="error">{error}</div> : ''}
-            <div className="button_div">
-              <input className="ibutton" type="submit" value="LOGIN" />
-            </div>
-          </div>
-        </div>
-      </form>
+        </form>
+      </Container>
     </div>
   );
 }
