df844dcb1015c052dc50d3cbd17e6ceed5570751
13prashant <pacific1392@gmail.com>
2022-11-01 06:52:42 +0530

    added loader to button

--- src/components/ui/WarehouseButton.js
+++ src/components/ui/WarehouseButton.js
@@ -1,25 +1,46 @@
 import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import CircularProgress from '@mui/material/CircularProgress';
+import { green } from '@mui/material/colors';
 
 export default function WarehouseButton({
   text,
   sm,
   success,
   warning,
+  loading,
   ...rest
 }) {
   const warehouseButtonStyle = {
     border: 'none',
     borderRadius: '0.3rem',
     padding: `${sm ? '0.3125rem 1rem' : '0.625rem 1rem'}`,
-    backgroundColor: `${success ? '#3bb077' : warning ? '#ea1010' : '#3f51b5'}`,
+    backgroundColor: `${
+      loading ? 'grey' : success ? '#3bb077' : warning ? '#ea1010' : '#3f51b5'
+    }`,
     color: '#ffffff',
-    cursor: 'pointer',
+    cursor: `${loading ? 'unset' : 'pointer'}`,
   };
 
   return (
-    <button {...rest} style={warehouseButtonStyle}>
-      {text}
-    </button>
+    <Box sx={{ m: 1, position: 'relative' }}>
+      <button {...rest} style={warehouseButtonStyle} disabled={loading}>
+        {text}
+      </button>
+      {loading && (
+        <CircularProgress
+          size={24}
+          sx={{
+            color: green[500],
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            marginTop: '-12px',
+            marginLeft: '-12px',
+          }}
+        />
+      )}
+    </Box>
   );
 }
 
